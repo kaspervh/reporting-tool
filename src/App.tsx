8361c9d9e1104722887a4cd052da566e0afcb734
kaspervh <kaspervhauschildt@protonmail.com>
2020-09-01 11:58:03 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React, {useMemo} from 'react';
+import React, {useState} from 'react';
 import SearchTable from './components/SearchTable';
 import './App.css';
 
 function App() {
+  const [currentContent, setCurrentContent] = useState('approve');
+
   return (
     <div className="App">
      <div className="content-container">
+      <div className="tableMenu">
+          <select onChange={(e:any) => setCurrentContent(e.target.value)}>
+            <option value="approve">Godkend</option>
+            <option value="account">Konter</option>
+            <option value="release">Frigiv</option>
+            <option value="all">Alle bilag</option>
+            <option value="scan">Scannede bilag</option>
+          </select>
+        </div>
        <SearchTable/>
+       {currentContent === 'scan' ?
+        <div className="data-container">
+          <h1>diller</h1>
+        </div>
+        : '' 
+      }
      </div>
      <div className="pdf-container"></div>
     </div>
--- src/components/SearchTable.tsx
+++ src/components/SearchTable.tsx
@@ -1,11 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import Documents from '../stubs/mockData';
+import React, {useState} from 'react';
+import InvoiceHeader from '../stubs/mockData';
 import Paper from '@material-ui/core/Paper';
-import {FilteringState, IntegratedFiltering, SelectionState} from '@devexpress/dx-react-grid';
-import {Grid, DragDropProvider, Table, TableHeaderRow, TableColumnResizing, ColumnChooser, TableColumnVisibility, Toolbar, TableFilterRow, TableColumnReordering, TableSelection} from '@devexpress/dx-react-grid-material-ui';
+import {FilteringState, IntegratedFiltering, SelectionState, PagingState,} from '@devexpress/dx-react-grid';
+import {Grid, DragDropProvider, Table, TableHeaderRow, TableColumnResizing, ColumnChooser, TableColumnVisibility, Toolbar, TableFilterRow, TableColumnReordering, TableSelection,} from '@devexpress/dx-react-grid-material-ui';
+import { LinearProgress } from '@material-ui/core';
 
 const SearchTable = () => {
-  const documentLines:any = Documents.lines
+  const documentLines:any = InvoiceHeader
+
+  console.log(documentLines.lines)
 
   const [columns, setColumns] = useState([]);
   const [rows, setRows] = useState([]);
@@ -14,25 +17,16 @@ const SearchTable = () => {
   const [defaultHiddenColumnNames, setDefaultHiddenColumnNames] = useState([]);
   const [selection, setSelection] = useState([0]);
 
-  useEffect(() => {
-
-    // if columnsis empty, code will get the data from "db"
-    if(columns.length === 0){
-      let cols:any = []
-      const names:String[] = Object.keys(documentLines[0]);
-      for(let element of names){cols.push({name: element, title: element.toLocaleUpperCase()})}
-      setColumns(cols);
-    }
-    // sets the table rows based on "db" data
-    if(rows.length === 0){setRows(documentLines) }
-
-    
-    console.log(defaultColumnWidths)
-  },[columns, rows, defaultColumnWidths, defaultTableOrder, defaultHiddenColumnNames])
-
   const setTableData = (element:string) => {
     if(element === "defaultTableOrder"){
-      const order:any = Object.keys(documentLines[0])
+      let order:any = Object.keys(documentLines[0]);
+      let localOrder:any = localStorage.getItem('defaultTableOrder');
+      if(localOrder != null){
+        localOrder = JSON.parse(localOrder);
+        if(localOrder !== defaultTableOrder as any ){
+          order = localOrder;
+        }
+      }
       setDefaultTableOrder(order)
       return defaultTableOrder
     }
@@ -41,6 +35,14 @@ const SearchTable = () => {
       let cols:any = [];
       const names:String[] = Object.keys(documentLines[0]);
       for(let element of names){cols.push({ columnName: element, width: 100 })}
+      let columnWidths:any = localStorage.getItem('defaultColumnWidths');
+      if(columnWidths != null){
+        columnWidths = JSON.parse(columnWidths)
+        if(columnWidths !== defaultColumnWidths as any){
+          cols = columnWidths
+        }
+        
+      }
       setDefaultColumnWidths(cols);
       return defaultColumnWidths;
     }
@@ -51,14 +53,26 @@ const SearchTable = () => {
     }
 
     if(element === 'columns'){
-      let cols:any = []
+      let cols:any = [];
+      console.log(Object.keys(documentLines[0]))
       const names:String[] = Object.keys(documentLines[0]);
+      
       for(let element of names){cols.push({name: element, title: element.toLocaleUpperCase()})}
       setColumns(cols);
       return columns;
     }
 
-
+    if(element === 'defaultHiddenColumnNames'){
+      let columnNames:any = localStorage.getItem('defaultColumnNames');
+      if(columnNames != null ){
+        columnNames = JSON.parse(columnNames);
+        if(defaultHiddenColumnNames.length === 0 && columnNames.length !== 0 ){
+          console.log('here')
+          setDefaultHiddenColumnNames(columnNames);
+        }
+      }
+      return defaultHiddenColumnNames
+    }
   }
   
   const handleRowSelection = (sel:any) => {
@@ -69,33 +83,47 @@ const SearchTable = () => {
     }
   }
 
-  const setTableOrder = () => {
-    if(defaultTableOrder.length === 0){
-      const order:any = Object.keys(documentLines[0])
-      setDefaultTableOrder(order)
-      return defaultTableOrder
-    }
+  const saveTableOrder = (tableOrder:any) => {
+    localStorage.setItem('defaultTableOrder', JSON.stringify(defaultTableOrder));
+    setDefaultTableOrder(tableOrder);
   }
-  
-  
+
+  const saveHiddenColumns = (columnNames:any) => {
+    localStorage.setItem('defaultColumnNames', JSON.stringify(columnNames));
+    setDefaultHiddenColumnNames(columnNames);
+  }
+
+  const saveColumnWidths = (columnWidth:any) => {
+    localStorage.setItem('defaultColumnWidths', JSON.stringify(columnWidth));
+    setDefaultColumnWidths(columnWidth);
+  }
+
+
   return(
     <div className="table-container">
+      
       <Paper>
         <Grid rows={rows.length === 0 ? setTableData("rows") : rows } columns={columns.length === 0 ? setTableData("columns") : columns}>
           <FilteringState defaultFilters={[]} />
           <IntegratedFiltering />
           <DragDropProvider />
+          
+          <PagingState defaultCurrentPage={0} pageSize={5} />
           <SelectionState selection={selection} onSelectionChange={(sel:any) => handleRowSelection(sel)} />
           <Table />
-          <TableColumnReordering defaultOrder={defaultTableOrder.length ===0 ? setTableData("defaultTableOrder") : defaultTableOrder} onOrderChange={(tableOrder:any) => setDefaultTableOrder(tableOrder)}/>
-          <TableColumnVisibility defaultHiddenColumnNames={defaultHiddenColumnNames} onHiddenColumnNamesChange={(columnNames:any) => setDefaultHiddenColumnNames(columnNames)}/>
-          <TableColumnResizing defaultColumnWidths={defaultColumnWidths.length === 0 ? setTableData("defaultColumnWidhs") : defaultColumnWidths} onColumnWidthsChange={(widths:any) => setDefaultColumnWidths(widths)}/>
+          <TableColumnReordering defaultOrder={defaultTableOrder.length ===0 ? setTableData("defaultTableOrder") : defaultTableOrder} onOrderChange={(tableOrder:any) => saveTableOrder(tableOrder)}/>
+          <TableColumnVisibility defaultHiddenColumnNames={setTableData('defaultHiddenColumnNames')} onHiddenColumnNamesChange={(columnNames:any) => saveHiddenColumns(columnNames)}/>
+          <TableColumnResizing defaultColumnWidths={defaultColumnWidths.length === 0 ? setTableData("defaultColumnWidhs") : defaultColumnWidths} onColumnWidthsChange={(widths:any) => saveColumnWidths(widths)}/>
           <TableSelection selectByRowClick highlightRow showSelectionColumn={false} />
+          
+          
           <Toolbar />
           <ColumnChooser />
-          <TableHeaderRow />
-          <TableFilterRow />
           
+          <TableHeaderRow />
+
+          <TableFilterRow />
+
         </Grid>
       </Paper>
     </div>
